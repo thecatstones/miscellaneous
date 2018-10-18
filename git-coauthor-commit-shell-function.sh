5e71b1b1d92dd3af598a81437d41fdd99838ef5b
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-18 10:47:23 -0700

    Added more detailed instructions.
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- git-coauthor-commit-shell-function.sh
+++ git-coauthor-commit-shell-function.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 # Shell function/alias for easy multi-author commits.
-# Usage: `$ cgap {COMMIT_MESSAGE}`
+
+# Instructions
+# 1. In .bashrc or .zshrc, source this file or copy and paste its contents.
+#   - eg: `source PATH/TO/git-coauthor-commit-shell-function.sh`
+# 2. Usage: `$ cgap {COMMIT_MESSAGE}`
 
 function git_add_commit_push_catstones {
   git add -A
