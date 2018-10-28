4d582a2262cf11ffdd780be6f3dd1c888cc1e92f
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-28 03:14:21 +0000

    added scripts
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

---
+++ scripts/gdrive-dl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Download file from google drive
+# Usage: `bash gdrive-dl.sh <fileid> <filename>`
+
+fileid="$1"
+filename="$2"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+rm cookie
